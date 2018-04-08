d49b8613f21bfaa10e50635940c97c4c8b35b3e9
Lilith Tundrus <echofox123@gmail.com>
2018-04-08 01:04:50 -0400

    stopped the timers for errors on decode

--- src/bot/commands/decodeHandler.ts
+++ src/bot/commands/decodeHandler.ts
@@ -8,14 +8,17 @@ export default async function startHandler(ctx) {
     ctx.logger.info(`decode from ${JSON.stringify(ctx.message.from)}`);
     let messageToDecode: string = ctx.message.text.substring(8);
     if (messageToDecode.length < 1) {
-        return ctx.reply(`Please give a message to decode`);
+        return ctx.reply(`Please give a message to decode`)
+        .then(() => ctx.time('decode error message sent!'));
     } else {
         // Check if the message is even something we need to decode here!
         let stringFromZeroWidth = zeroWidthToString.default(messageToDecode);
         console.log(stringFromZeroWidth.length);
         // The length returned is always at the least 2???
         if (stringFromZeroWidth.length <= 2) {
-            return ctx.reply(`Given message did not contain zero-width characters to decode, make sure you copied the entire encoded message!`);
+            return ctx.reply(`Given message did not contain zero-width characters to decode, make sure you copied the entire encoded message!`)
+            .then(() => ctx.time('decode error message sent!'));
+
         }
         return ctx.reply(`Decoded message: ${stringFromZeroWidth}`)
             .then(() => ctx.time('reply message sent!'));
