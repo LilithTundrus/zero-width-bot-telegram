aa32ae8b3e2f0de10ded349c70fe28dda926469c
Lilith Tundrus <echofox123@gmail.com>
2018-04-07 23:02:37 -0400

    Converted this to typescript (very poorly)

---
+++ src/lib/zeroWidthToUsername.ts
@@ -0,0 +1,20 @@
+const zeroWidthToBinary = (stringToCOnvert: string) => (
+    stringToCOnvert.split('﻿').map((char) => { // invisible &#65279;
+        if (char === '​') { // invisible &#8203;
+            return '1';
+        } else if (char === '‌') { // invisible &#8204;
+            return '0';
+        }
+        return ' '; // split up binary with spaces;
+    }).join('')
+);
+
+const binaryToText = (stringToCOnvert: string) => (
+    stringToCOnvert.split(' ').map(num => String.fromCharCode(parseInt(num, 2))).join('')
+);
+
+export default (zeroWidthUsername) => {
+    const binaryUsername = zeroWidthToBinary(zeroWidthUsername);
+    const textUsername = binaryToText(binaryUsername);
+    return textUsername;
+};
