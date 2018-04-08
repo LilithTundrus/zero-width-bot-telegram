207521f3da7e0d17a3373c0822f823bb12773978
Lilith Tundrus <echofox123@gmail.com>
2018-04-07 22:19:06 -0400

    added encode to handled command set

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -2,10 +2,13 @@
 import Composer from 'telegraf';
 import startHandler from './startHandler';
 import processInfHandler from './processInfoHandler';
+import encdoeHandler from './encodeHandler';
 
 const composer = new Composer();
 
 composer.command('start', startHandler);
 composer.command('procinfo', processInfHandler);
+composer.command('encode', encdoeHandler);
+
 
 export = composer;
