7c673cd1377f61a25fddf1d47023be84a1094baf
Lilith Tundrus <echofox123@gmail.com>
2018-04-07 23:36:37 -0400

    changed variable names

--- src/lib/zeroWidthToUsername.ts
+++ src/lib/zeroWidthToUsername.ts
@@ -1,5 +1,5 @@
-const zeroWidthToBinary = (stringToCOnvert: string) => (
-    stringToCOnvert.split('﻿').map((char) => { // invisible &#65279;
+const zeroWidthToBinary = (stringToConvert: string) => (
+    stringToConvert.split('﻿').map((char) => { // invisible &#65279;
         if (char === '​') { // invisible &#8203;
             return '1';
         } else if (char === '‌') { // invisible &#8204;
@@ -9,14 +9,14 @@ const zeroWidthToBinary = (stringToCOnvert: string) => (
     }).join('')
 );
 
-const binaryToText = (stringToCOnvert: string) => (
-    stringToCOnvert.split(' ').map(num => String.fromCharCode(parseInt(num, 2))).join('')
+const binaryToText = (stringToConvert: string) => (
+    stringToConvert.split(' ').map(num => String.fromCharCode(parseInt(num, 2))).join('')
 );
 
-const zeroWidthToString = (zeroWidthUsername) => {
-    const binaryUsername = zeroWidthToBinary(zeroWidthUsername);
-    const textUsername = binaryToText(binaryUsername);
-    return textUsername;
+const zeroWidthToString = (zeroWidthString) => {
+    const binaryString = zeroWidthToBinary(zeroWidthString);
+    const textString = binaryToText(binaryString);
+    return textString;
 };
 
 export default zeroWidthToString;
