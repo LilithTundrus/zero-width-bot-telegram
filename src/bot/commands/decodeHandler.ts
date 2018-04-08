99a65fc227b72c70522d7fd8f0164456a0ab0a4a
Lilith Tundrus <echofox123@gmail.com>
2018-04-08 00:47:14 -0400

    added a basic check for any actual decode results from the function, replying to the user if nothing is found

--- src/bot/commands/decodeHandler.ts
+++ src/bot/commands/decodeHandler.ts
@@ -6,12 +6,17 @@ export default async function startHandler(ctx) {
     ctx.resetTimer();
     ctx.time('Got decode message...');
     ctx.logger.info(`decode from ${JSON.stringify(ctx.message.from)}`);
-    // Check if the message is even something we need to decode here!
     let messageToDecode: string = ctx.message.text.substring(8);
     if (messageToDecode.length < 1) {
         return ctx.reply(`Please give a message to decode`);
     } else {
+        // Check if the message is even something we need to decode here!
         let stringFromZeroWidth = zeroWidthToString.default(messageToDecode);
+        console.log(stringFromZeroWidth.length);
+        // The length returned is always at the least 2???
+        if (stringFromZeroWidth.length <= 2) {
+            return ctx.reply(`Given message did not contain zero-width characters to decode, make sure you copied the entire encoded message!`);
+        }
         return ctx.reply(`Decoded message: ${stringFromZeroWidth}`)
             .then(() => ctx.time('reply message sent!'));
     }
