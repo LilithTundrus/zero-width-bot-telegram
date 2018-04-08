b419977205c6df561a4216cb0bb83811359074ed
Lilith Tundrus <echofox123@gmail.com>
2018-04-07 23:15:17 -0400

    fixed typo, added decode handler reference

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -2,13 +2,16 @@
 import Composer from 'telegraf';
 import startHandler from './startHandler';
 import processInfHandler from './processInfoHandler';
-import encdoeHandler from './encodeHandler';
+import encodeHandler from './encodeHandler';
+import decodeHandler from './decodeHandler';
 
 const composer = new Composer();
 
 composer.command('start', startHandler);
 composer.command('procinfo', processInfHandler);
-composer.command('encode', encdoeHandler);
+composer.command('encode', encodeHandler);
+composer.command('decode', decodeHandler);
+
 
 
 export = composer;
