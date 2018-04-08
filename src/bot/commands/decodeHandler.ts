db1154eabe1f6672d4f94af6f8063d878eb456e5
Lilith Tundrus <echofox123@gmail.com>
2018-04-08 00:13:27 -0400

    decode and encode handlers now take very basic arguments

--- src/bot/commands/decodeHandler.ts
+++ src/bot/commands/decodeHandler.ts
@@ -5,9 +5,14 @@ import * as zeroWidthToString from '../../lib/zeroWidthToUsername';
 export default async function startHandler(ctx) {
     ctx.resetTimer();
     ctx.time('Got decode message...');
-    ctx.logger.info(`encode from ${JSON.stringify(ctx.message.from)}`)
-    let stringFromZeroWidth = zeroWidthToString.default('This contains your message ‌﻿​﻿​﻿​﻿‌﻿‌﻿​﻿​﻿‍﻿‌﻿​﻿​﻿‌﻿​﻿‌﻿‌﻿​﻿‍﻿‌﻿​﻿​﻿‌﻿​﻿​﻿‌﻿​﻿‍﻿‌﻿​﻿​﻿​﻿‌﻿‌﻿‌﻿‌﻿‍﻿‌﻿​﻿​﻿‌﻿​﻿​﻿‌﻿‌﻿‍﻿‌﻿​﻿​﻿‌﻿‌﻿​﻿‌﻿​﻿‍﻿‌﻿​﻿‌﻿​﻿​﻿​﻿​﻿​﻿‍﻿‌﻿​﻿​﻿​﻿‌﻿​﻿‌﻿‌﻿‍﻿‌﻿​﻿​﻿‌﻿‌﻿​﻿‌﻿​﻿‍﻿‌﻿​﻿​﻿​﻿‌﻿‌﻿​﻿​﻿‍﻿‌﻿​﻿​﻿​﻿‌﻿​﻿‌﻿‌')
-    // console.log(`This contains your message ${zeroWidthString}`)
-    return ctx.reply(`This contains your message ${stringFromZeroWidth}`)
-        .then(() => ctx.time('reply message sent!'));
+    ctx.logger.info(`decode from ${JSON.stringify(ctx.message.from)}`);
+    // Check if the message is even something we need to decode here!
+    let messageToDecode: string = ctx.message.text.substring(8);
+    if (messageToDecode.length < 1) {
+        return ctx.reply(`Please give a message to decode`);
+    } else {
+        let stringFromZeroWidth = zeroWidthToString.default(messageToDecode);
+        return ctx.reply(`Decoded message: ${stringFromZeroWidth}`)
+            .then(() => ctx.time('reply message sent!'));
+    }
 }
--- src/bot/commands/encodeHandler.ts
+++ src/bot/commands/encodeHandler.ts
@@ -1,13 +1,20 @@
 'use strict';
-import * as stringToZeroWidth from '../../lib/usernameToZeroWidth'
+import * as stringToZeroWidth from '../../lib/usernameToZeroWidth';
 
 // handle /encode command calls
-export default async function startHandler(ctx) {
+export default async function encodeHandler(ctx) {
     ctx.resetTimer();
-    ctx.time('Got encode message...');
-    ctx.logger.info(`encode from ${JSON.stringify(ctx.message.from)}`)
-    let zeroWidthString = stringToZeroWidth.default('simple_test')
+    ctx.time('Got encode message request...');
+    ctx.logger.info(`encode request from ${JSON.stringify(ctx.message.from)}`);
+    let messageToEncode: string = ctx.message.text.substring(8);
+    if (messageToEncode.length < 1) {
+        return ctx.reply(`Please give a message to encode`);
+    } else {
+    // We want to be able to take a placeholder string from the user
+    console.log(messageToEncode)
+    let zeroWidthString = stringToZeroWidth.default(messageToEncode);
     // console.log(`This contains your message ${zeroWidthString}`)
-    return ctx.reply(`This contains your message ${zeroWidthString}`)
-        .then(() => ctx.time('reply message sent!'));
+    return ctx.reply(`AAAA${zeroWidthString}`)
+        .then(() => ctx.time('encoded reply message sent!'));
+    }
 }
