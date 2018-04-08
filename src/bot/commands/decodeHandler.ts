997555182a61ebc80d91669f38c1aa573125af96
Lilith Tundrus <echofox123@gmail.com>
2018-04-08 15:59:12 -0400

    Adjusted some formatting, added related comments.

--- src/bot/commands/decodeHandler.ts
+++ src/bot/commands/decodeHandler.ts
@@ -1,5 +1,5 @@
 'use strict';
-import * as zeroWidthToString from '../../lib/zeroWidthToUsername';
+import * as zeroWidthToString from '../../lib/zeroWidthToString';
 
 // handle /encode command calls
 export default async function startHandler(ctx) {
@@ -7,18 +7,19 @@ export default async function startHandler(ctx) {
     ctx.time('Got decode message...');
     ctx.logger.info(`decode from ${JSON.stringify(ctx.message.from)}`);
     let messageToDecode: string = ctx.message.text.substring(8);
+    // make sure the /decode argument has a string to decode
     if (messageToDecode.length < 1) {
         return ctx.reply(`Please give a message to decode`)
-        .then(() => ctx.time('decode error message sent!'));
+            .then(() => ctx.time('decode error message sent!'));
     } else {
-        // Check if the message is even something we need to decode here!
+        // Check if the message is even something we need to decode!
         let stringFromZeroWidth = zeroWidthToString.default(messageToDecode);
         console.log(stringFromZeroWidth.length);
-        // The length returned is always at the least 2???
+        // The length returned is always at the least 2, even with a single character provided which is weird
         if (stringFromZeroWidth.length <= 2) {
+            // Message does not contain anything we can decode
             return ctx.reply(`Given message did not contain zero-width characters to decode, make sure you copied the entire encoded message!`)
-            .then(() => ctx.time('decode error message sent!'));
-
+                .then(() => ctx.time('decode error message sent!'));
         }
         return ctx.reply(`Decoded message: ${stringFromZeroWidth}`)
             .then(() => ctx.time('reply message sent!'));
