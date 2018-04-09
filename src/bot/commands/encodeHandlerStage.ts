810636bfd1909e6ac24afce587dc6a85465b4bf5
Lilith Tundrus <echofox123@gmail.com>
2018-04-08 23:44:05 -0400

    some cleanup, less debugging code and more TS friendly function typings

--- src/bot/commands/encodeHandlerStage.ts
+++ src/bot/commands/encodeHandlerStage.ts
@@ -2,50 +2,46 @@ import * as stringToZeroWidth from '../../lib/stringToZeroWidth';
 import Stage from 'telegraf/stage';
 import Scene from 'telegraf/scenes/base';
 
-// on the 'encode' command, a user is brought into a scene to craft a container and message
 const { enter, leave } = Stage;
 const encodeScene = new Scene('encode');
 
 // this is probably the absolute worst way to do this
-
 let tempUserDataArray = [];
 
+// on the 'encode' command, a user is brought into a scene to craft a container and message
 encodeScene.enter((ctx) => {
-    tempUserDataArray.push(generateTempObj(ctx.message.from.id));
-    // we may need to create a local object here if we're storing any amount of temporary user data
-    console.log(ctx.chat)
-    return ctx.reply('You are in the decode scene now! use /back to leave. use /c to set container message, use /m to set the hideen message. use /done to get your container with the hidden message');
-    // send the text markup
-    // maybe we run a .next chain?
+    // generate an object per user call 
+    tempUserDataArray.push(createTemUser(ctx.message.from.id));
+    return ctx.reply('You are in the encode command now! use /back to leave. use /c + message to set container message, use /m + message to set the hideen message. use /done to get your container with the hidden message.');
+    // send the keyboard markup
 })
 
 encodeScene.leave((ctx) => {
     removeTempUser(ctx.chat.id);
-    return ctx.reply('You just left the encode scene, all base commands are now available!');
+    return ctx.reply('You just left the encode command, all base commands are now available!');
 });
 
 // TODO: if any base commands are given, let the user know they're not in the base scene!
+// TODO: at the end of /c and /m, check if the user has both data points and send the 
+// message without the need for /done to be used
 encodeScene.command('back', leave());
 
 encodeScene.command('c', (ctx) => {
     let container = ctx.message.text.substring(3).trim();
-    console.log(container);
     if (container.length < 1) {
         return ctx.reply('please give a message to be a container for the secret message!');
     }
-    setUserContainer(container, ctx.message.from.id);
     // assign the temp variable for the message
+    return setUserContainer(container, ctx.message.from.id);
 })
 
 encodeScene.command('m', (ctx) => {
     let message = ctx.message.text.substring(3).trim();
-    console.log(message);
     if (message.length < 1) {
-        return ctx.reply('please give a message to encode!');
+        return ctx.reply('please give a message to encode into the container!');
     }
     // assign the temp variable for the message
-    setUserMessage(message, ctx.message.from.id);
-    // check if both message and container have been set
+    return setUserMessage(message, ctx.message.from.id);
 })
 
 encodeScene.command('done', (ctx) => {
@@ -56,12 +52,12 @@ encodeScene.command('done', (ctx) => {
         ctx.reply('Got it, your message is below:');
         return ctx.reply(`${userData.container}${zeroWidthString}`);
     } else {
-        return ctx.reply('Please make sure you have set a container and message using the /c and /m command')
+        return ctx.reply('Please make sure you have set a container and message using the /c and /m command');
     }
 })
 
-
-function generateTempObj(teleID) {
+// CRUD functions for temporary user objects
+function createTemUser(teleID: string) {
     let tempUserDataObj = {
         container: '',
         message: '',
@@ -74,7 +70,6 @@ function setUserContainer(container: string, teleID: string) {
     for (let userData of tempUserDataArray) {
         if (userData.teleID == teleID) {
             userData.container = container;
-            console.log(userData)
         }
     }
 }
@@ -83,12 +78,11 @@ function setUserMessage(message: string, teleID: string) {
     for (let userData of tempUserDataArray) {
         if (userData.teleID == teleID) {
             userData.message = message;
-            console.log(userData)
         }
     }
 }
 
-function getUserData(teleID) {
+function getUserData(teleID: string) {
     for (let userData of tempUserDataArray) {
         if (userData.teleID == teleID) {
             return userData;
@@ -96,11 +90,12 @@ function getUserData(teleID) {
     }
 }
 
-function removeTempUser(teleID) {
+function removeTempUser(teleID: string) {
+    // reverse lookup to prevent array corruption/mistmatching
     for (var i = tempUserDataArray.length - 1; i >= 0; --i) {
         if (tempUserDataArray[i].teleID == teleID) {
             tempUserDataArray.splice(i, 1);
-            console.log('removed user from temp list!');
+            console.log(`Removed user ${teleID} from the temp array`);
         }
     }
 }
