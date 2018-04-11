093c1e57723457ea8e8742a112563e3c912dc7ac
Lilith Tundrus <echofox123@gmail.com>
2018-04-10 20:22:47 -0400

    semicolon fix

--- src/bot/commands/startHandler.ts
+++ src/bot/commands/startHandler.ts
@@ -4,7 +4,7 @@ import { mainMenuKeyboard } from "../keyboardMarkups";
 export default async function startHandler(ctx) {
     ctx.resetTimer();
     ctx.time('Sending welcome message...');
-    ctx.logger.info(`Start from ${JSON.stringify(ctx.message.from)}`)
+    ctx.logger.info(`Start from ${JSON.stringify(ctx.message.from)}`);
     return ctx.reply(`Hi @${ctx.message.from.username} \n\nI'm a bot that can help you encode/decode messages hidden in plain sight! I can also help you detect if a string has/has not been fingerprinted with zero-width characters.\n\nNot sure what any of this means? Check this out: https://medium.com/@umpox/be-careful-what-you-copy-invisibly-inserting-usernames-into-text-with-zero-width-characters-18b4e6f17b66\n\nPlease note this is still in development!`, mainMenuKeyboard)
         .then(() => ctx.time('Message sent!'));
 }
