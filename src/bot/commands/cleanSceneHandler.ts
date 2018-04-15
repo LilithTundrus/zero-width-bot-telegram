729666128821391922c92ddce3bf34c4748a3473
Lilith Tundrus <echofox123@gmail.com>
2018-04-14 23:57:49 -0400

    got text handling in the absolute worst way possible

--- src/bot/commands/cleanSceneHandler.ts
+++ src/bot/commands/cleanSceneHandler.ts
@@ -1,5 +1,6 @@
 'use strict';
-import * as zeroWidthToString from '../../lib/zeroWidthToString';
+import zeroWidthToString from '../../lib/zeroWidthToString';
+import stringToZeroWidth from '../../lib/stringToZeroWidth';
 import Stage from 'telegraf/stage';
 import Scene from 'telegraf/scenes/base';
 import { cleanKeyboard } from '../keyboardMarkups';
@@ -19,7 +20,7 @@ cleanScene.enter((parentCtx) => {
 })
 
 cleanScene.leave((parentCtx) => {
-    parentCtx.telegram.editMessageText(parentCtx.chat.id, parentCtx.session.messageToEdit, null, 'ℹ️ You just left the decode command, all base commands are now available using /menu!');
+    parentCtx.telegram.editMessageText(parentCtx.chat.id, parentCtx.session.messageToEdit, null, 'ℹ️ You just left the clean command, all base commands are now available using /menu!');
 });
 
 cleanScene.command('back', leave());
@@ -32,5 +33,88 @@ cleanScene.action('exit', (ctx) => {
     ctx.answerCbQuery(ctx.callbackQuery.data);
     return ctx.scene.leave();
 });
+
+cleanScene.action('message', (ctx) => {
+    let messageToSend = 'ℹ️ Send me a copy-pasted set of text to remove zero-width characters!';
+    // 'answer' the CB, making the loading icon go away
+    ctx.answerCbQuery(ctx.callbackQuery.data);
+    if (ctx.session.lastSentMessage !== messageToSend) {
+        ctx.session.lastSentMessage = messageToSend;
+        return ctx.telegram.editMessageText(ctx.chat.id, ctx.session.messageToEdit, null, messageToSend, cleanKeyboard);
+    }
+});
+
+cleanScene.action('file', (ctx) => {
+    let messageToSend = 'ℹ️ Send a file to remove zero-width characters!';
+    // 'answer' the CB, making the loading icon go away
+    ctx.answerCbQuery(ctx.callbackQuery.data);
+    if (ctx.session.lastSentMessage !== messageToSend) {
+        ctx.session.lastSentMessage = messageToSend;
+        return ctx.telegram.editMessageText(ctx.chat.id, ctx.session.messageToEdit, null, messageToSend, cleanKeyboard);
+    }
+});
+
+// on text, attempt a decode or scene change
+cleanScene.on('text', (ctx) => {
+    let userMessage = ctx.message.text.trim();
+    // if text is a command, go to that scene!!
+    if (userMessage == '💊 Clean') {
+        // Doesn't work more than once annoyingly
+        let messageToSend = 'ℹ️ You are already in the 💊 Clean command!';
+        if (ctx.session.lastSentMessage !== messageToSend) {
+            ctx.session.lastSentMessage = messageToSend;
+            return ctx.telegram.editMessageText(ctx.chat.id, ctx.session.messageToEdit, null, messageToSend, cleanKeyboard);
+        }
+    } else if (userMessage == '✉️ Encode') {
+        return ctx.scene.leave().then(() => {
+            return ctx.scene.enter('encode');
+        });
+    } else if (userMessage == '📨 Decode') {
+        // Doesn't work more than once annoyingly
+        return ctx.scene.leave().then(() => {
+            return ctx.scene.enter('decode');
+        });
+    } else if (userMessage == '🔍 Detect') {
+        return ctx.scene.leave().then(() => {
+            return ctx.scene.enter('detect');
+        });
+    }
+    // Check if the message has zero-wdith characters
+    if (zeroWidthCheck(userMessage) == false) {
+        // Message does not contain anything we can detect
+        let messageToSend = `⚠️ Given message did NOT contain zero-width characters`;
+        if (ctx.session.lastSentMessage !== messageToSend) {
+            ctx.session.lastSentMessage = messageToSend;
+            return ctx.telegram.editMessageText(ctx.chat.id, ctx.session.messageToEdit, null, messageToSend, cleanKeyboard);
+        }
+    } else {
+        let stringFromZeroWidth = zeroWidthToString(userMessage);
+        let fixedString = userMessage.split('').filter(function(char) {
+            if (char == "﻿" || /* <feff> */
+                char == "​" || /* <200b> */
+                char == "‌" || /* <200c> */
+                char == "‍"    /* <200d> */
+              ) {
+              return false;
+            }
+            return true;
+          }).join('');
+        let messageToSend = `✅ String with zero-wdith characters removed: ${fixedString}`;
+        if (ctx.session.lastSentMessage !== messageToSend) {
+            ctx.session.lastSentMessage = messageToSend;
+            return ctx.telegram.editMessageText(ctx.chat.id, ctx.session.messageToEdit, null, messageToSend, cleanKeyboard);
+        }
+    }
+})
+
+function zeroWidthCheck(textToCheck: string) {
+    let stringFromZeroWidth = zeroWidthToString(textToCheck);
+    // The length returned is always at the least 2, even with a single character provided which is weird
+    if (stringFromZeroWidth.length <= 2) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 export default cleanScene;
