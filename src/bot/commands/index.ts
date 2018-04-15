27d2d1e9c6e9835f87886cb4b6f7ac904daf8546
Lilith Tundrus <echofox123@gmail.com>
2018-04-14 23:11:26 -0400

    added support for 💊 Clean command

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -12,9 +12,11 @@ import processinfoHandler from './processInfoHandler';
 import encodeScene from './encodeHandlerScene';
 import detectScene from './detectHandlerScene';
 import decodeScene from './decodeHandlerScene';
+import cleanScene from './cleanSceneHandler';
+
 
 // Literally set the stages for the composer to user as middleware for certain commands
-const stage = new Stage([encodeScene, detectScene, decodeScene]);
+const stage = new Stage([encodeScene, detectScene, decodeScene, cleanScene]);
 const { enter, leave } = Stage;
 
 const composer = new Composer();
@@ -29,11 +31,15 @@ composer.command('procinfo', processinfoHandler);
 composer.command('decode', enter('decode'));
 composer.command('encode', enter('encode'));
 composer.command('detect', enter('detect'));
+composer.command('clean', enter('clean'));
+
 
 
 // listen for specific commands from the main menu
 composer.hears('🔍 Detect', enter('detect'));
 composer.hears('✉️ Encode', enter('encode'));
 composer.hears('📨 Decode', enter('decode'));
+composer.hears('💊 Clean', enter('clean'));
+
 
 export = composer;
