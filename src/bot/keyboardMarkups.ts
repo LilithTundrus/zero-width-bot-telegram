4ad3250b6d7789a00df14d3fb1e7bc4342015d33
Lilith Tundrus <echofox123@gmail.com>
2018-04-15 15:56:45 -0400

    Fixed formatting and logical order

--- src/bot/keyboardMarkups.ts
+++ src/bot/keyboardMarkups.ts
@@ -1,22 +1,10 @@
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup')
 
-
-const encodeKeyboard = Extra.HTML().markup((m) =>
-    m.inlineKeyboard([
-        m.callbackButton('✉️ Set Message', 'message'),
-        m.callbackButton('📁 Set Container', 'container'),
-        m.callbackButton('📄 Encode File', 'file'),
-        m.callbackButton('✅ Create', 'done'),
-        m.callbackButton('❌ Exit', 'exit')],
-        { columns: 2 }));
-
-
 const mainMenuKeyboard = Markup.keyboard([
     ['🔍 Detect', '💊 Clean'],
     ['✉️ Encode', '📨 Decode'],
 ]).resize().extra();
-
 
 const detectKeyboard = Extra.HTML()
     .markup((m) =>
@@ -27,34 +15,44 @@ const detectKeyboard = Extra.HTML()
             { columns: 2 })
     );
 
-const decodeKeyboard = Extra.HTML()
-.markup((m) =>
+const encodeKeyboard = Extra.HTML().markup((m) =>
     m.inlineKeyboard([
-        m.callbackButton('✉️ Deocde From Message', 'message'),
-        m.callbackButton('📄 Deocde From File', 'file'),
+        m.callbackButton('✉️ Set Message', 'message'),
+        m.callbackButton('📁 Set Container', 'container'),
+        m.callbackButton('📄 Encode File', 'file'),
+        m.callbackButton('✅ Create', 'done'),
         m.callbackButton('❌ Exit', 'exit')],
         { columns: 2 })
 );
+
+const decodeKeyboard = Extra.HTML()
+    .markup((m) =>
+        m.inlineKeyboard([
+            m.callbackButton('✉️ Deocde From Message', 'message'),
+            m.callbackButton('📄 Deocde From File', 'file'),
+            m.callbackButton('❌ Exit', 'exit')],
+            { columns: 2 })
+    );
 
 const cleanKeyboard = Extra.HTML()
-.markup((m) =>
-    m.inlineKeyboard([
-        m.callbackButton('✉️ Clean Message', 'message'),
-        m.callbackButton('📄 Clean File', 'file'),
-        m.callbackButton('❌ Exit', 'exit')],
-        { columns: 2 })
-);
+    .markup((m) =>
+        m.inlineKeyboard([
+            m.callbackButton('✉️ Clean Message', 'message'),
+            m.callbackButton('📄 Clean File', 'file'),
+            m.callbackButton('❌ Exit', 'exit')],
+            { columns: 2 })
+    );
 
 const adminKeyboard = Extra.HTML()
-.markup((m) =>
-    m.inlineKeyboard([
-        m.callbackButton('ℹ️ Process Info', 'procinfo'),
-        m.callbackButton('🗒 Logs', 'logs'),
-        m.callbackButton('🔅 Restart bot', 'restart'),
-        m.callbackButton('📈 Stats', 'stats'),
-        m.callbackButton('❌ Exit', 'exit')
-    ],
-        { columns: 2 })
-);
+    .markup((m) =>
+        m.inlineKeyboard([
+            m.callbackButton('ℹ️ Process Info', 'procinfo'),
+            m.callbackButton('🗒 Logs', 'logs'),
+            m.callbackButton('🔅 Restart bot', 'restart'),
+            m.callbackButton('📈 Stats', 'stats'),
+            m.callbackButton('❌ Exit', 'exit')
+        ],
+            { columns: 2 })
+    );
 
 export { encodeKeyboard, mainMenuKeyboard, detectKeyboard, decodeKeyboard, cleanKeyboard }
